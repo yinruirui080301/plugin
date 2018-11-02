2cddac5a381d8382da9402aa9c40e39bbfd793a4
yinruirui080301 <2732181760@qq.com>
2018-11-02 08:58:04 +0800

    new Circle

--- src/com/lsf/impl/Circle.java
+++ src/com/lsf/impl/Circle.java
@@ -3,11 +3,13 @@ package com.lsf.impl;
 import com.lsf.graphic.Shape;
 
 public class Circle implements Shape {
-
+       private int r;
+       public Circle(){r=1;}
+       public Circle(int r){this.r=r;}
 	@Override
 	public double area() {
 		// TODO Auto-generated method stub
-		return 3;
+		return 3.14*r*r;
 	}
 
 }
